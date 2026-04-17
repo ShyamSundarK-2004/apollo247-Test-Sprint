6381ebe4737d1d5132b09f928cb0d1446b2b0b88
Priyanka <priyankagajendran17@gmail.com>
2026-04-17 08:25:51 +0530

    MyAccount updated

--- src/main/java/com/apollo247/testing/pages/ManageFamilyPage.java
+++ src/main/java/com/apollo247/testing/pages/ManageFamilyPage.java
@@ -1,28 +1,43 @@
 package com.apollo247.testing.pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.SearchContext;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.PageFactory;
 
 public class ManageFamilyPage {
-	WebDriver driver;
-	WebDriverWait wait;
+
+    WebDriver driver;
+
+    // Constructor
+    public ManageFamilyPage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+
+    @FindBy(className = "ProfileNew_profileContainer__mUxKD")
+    private WebElement profileIcon;
+
+    @FindBy(xpath = "//span[contains(.,'Manage Family Members')]")
+    private WebElement manageFamilyMembers;
 
     @FindBy(xpath = "//span[contains(.,'Add New Profile')]")
-    private WebElement addNewProfileButton;
+    private WebElement addNewProfile;
 
     @FindBy(css = "[placeholder='First Name']")
-    private WebElement firstNameInput;
+    private WebElement firstName;
 
     @FindBy(css = "[placeholder='Last name']")
-    private WebElement lastNameInput;
+    private WebElement lastName;
 
     @FindBy(css = "[placeholder='dd/mm/yyyy']")
-    private WebElement dobInput;
+    private WebElement dob;
 
     @FindBy(xpath = "//span[.='Female']")
-    private WebElement femaleRadioButton;
+    private WebElement femaleGender;
 
     @FindBy(xpath = "//div[@class='AphSelect_select__MnPUj ']")
     private WebElement relationDropdown;
@@ -31,9 +46,100 @@ public class ManageFamilyPage {
     private WebElement motherOption;
 
     @FindBy(xpath = "//span[.='Save']")
-    private WebElement saveButton;
+    private WebElement saveBtn;
 
     @FindBy(xpath = "//span[.='CONFIRM']")
-    private WebElement confirmButton;
+    private WebElement confirmBtn;
 
+    // Getter Methods
+
+    public WebElement getProfileIcon() {
+        return profileIcon;
+    }
+
+    public WebElement getManageFamilyMembers() {
+        return manageFamilyMembers;
+    }
+
+    public WebElement getAddNewProfile() {
+        return addNewProfile;
+    }
+
+    public WebElement getFirstName() {
+        return firstName;
+    }
+
+    public WebElement getLastName() {
+        return lastName;
+    }
+
+    public WebElement getDob() {
+        return dob;
+    }
+
+    public WebElement getFemaleGender() {
+        return femaleGender;
+    }
+
+    public WebElement getRelationDropdown() {
+        return relationDropdown;
+    }
+
+    public WebElement getMotherOption() {
+        return motherOption;
+    }
+
+    public WebElement getSaveBtn() {
+        return saveBtn;
+    }
+
+    public WebElement getConfirmBtn() {
+        return confirmBtn;
+    }
+
+    // Business Methods
+
+    // Navigate to Manage Family Members
+    public void openManageFamilyMembers() {
+        profileIcon.click();
+        manageFamilyMembers.click();
+    }
+
+    // Click Add New Profile
+    public void clickAddNewProfile() {
+        addNewProfile.click();
+    }
+
+    // Enter details
+    public void enterFamilyMemberDetails(String fName, String lName, String dateOfBirth) {
+        firstName.sendKeys(fName);
+        lastName.sendKeys(lName);
+        dob.sendKeys(dateOfBirth);
+    }
+
+    // Select gender + relation
+    public void selectFemaleAndMother() {
+        femaleGender.click();
+        relationDropdown.click();
+        motherOption.click();
+    }
+
+    // Save member
+    public void saveFamilyMember() {
+        saveBtn.click();
+        confirmBtn.click();
+    }
+
+    // Full reusable flow
+    public void addFamilyMember(String fName, String lName, String dobValue) {
+        clickAddNewProfile();
+        enterFamilyMemberDetails(fName, lName, dobValue);
+        selectFemaleAndMother();
+        saveFamilyMember();
+    }
+
+    // Shadow DOM popup handling (kept same)
+    public void closePopup(SearchContext shadowRoot) {
+        shadowRoot.findElement(By.cssSelector("#close")).click();
+    }
 }
--- src/main/java/com/apollo247/testing/pages/MyAppointmentsPage.java
+++ src/main/java/com/apollo247/testing/pages/MyAppointmentsPage.java
@@ -1,15 +1,67 @@
 package com.apollo247.testing.pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.SearchContext;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class MyAppointmentsPage {
 
+    WebDriver driver;
+
+    // Constructor
+    public MyAppointmentsPage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this); // IMPORTANT
+    }
+
+
+    @FindBy(className = "ProfileNew_profileContainer__mUxKD")
+    private WebElement profileIcon;
+
     @FindBy(xpath = "//span[contains(.,'My Appointments')]")
-    private WebElement myAppointmentsLink;
+    private WebElement myAppointments;
 
-    // Used for verifying page content after navigation and after refresh
-    @FindBy(xpath = "//*[contains(text(),'Appointments') or contains(text(),'No Appointments')]")
-    private WebElement appointmentsTextElement;
+    // Getter Methods
 
-}  
+    public WebElement getProfileIcon() {
+        return profileIcon;
+    }
+
+    public WebElement getMyAppointments() {
+        return myAppointments;
+    }
+
+    // Business Logic
+
+    // Navigate to My Appointments
+    public void openMyAppointments() {
+        profileIcon.click();
+        myAppointments.click();
+    }
+
+    // Validate appointments page content
+    public boolean isAppointmentsPageDisplayed() {
+        String pageText = driver.getPageSource();
+        return pageText.contains("Appointments") || pageText.contains("No Appointments");
+    }
+
+    // Validate page after refresh
+    public boolean isPageLoadedAfterRefresh() {
+        return driver.findElement(
+                By.xpath("//*[contains(text(),'Appointments') or contains(text(),'No Appointments')]")
+        ).isDisplayed();
+    }
+
+    // Refresh page
+    public void refreshPage() {
+        driver.navigate().refresh();
+    }
+
+    // Shadow DOM popup close - reusable
+    public void closePopup(SearchContext shadowRoot) {
+        shadowRoot.findElement(By.cssSelector("#close")).click();
+    }
+}
