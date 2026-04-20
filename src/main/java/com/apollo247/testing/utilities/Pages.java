cd10dda53ce2e5da1edc81c482af599eb51c927a
Priyanka <priyankagajendran17@gmail.com>
2026-04-20 08:28:29 +0530

    Conflicts resolved

--- src/main/java/com/apollo247/testing/utilities/Pages.java
+++ src/main/java/com/apollo247/testing/utilities/Pages.java
@@ -3,24 +3,19 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 import com.apollo247.testing.pages.DashboardPage;
 import com.apollo247.testing.pages.LabTestPage;
-<<<<<<< HEAD
 
-import com.apollo247.testing.pages.HealthInsurancePage;
-=======
+//import com.apollo247.testing.pages.HealthInsurancePage;
 import com.apollo247.testing.pages.SearchDoctorPage;
 import com.apollo247.testing.pages.RadiologyPage;
 import com.apollo247.testing.pages.UploadPrescriptionPage;
 import com.apollo247.testing.pages.filterDocterPage;
-//import com.apollo247.testing.pages.ManageFamilyPage;
+import com.apollo247.testing.pages.ManageFamilyPage;
 import com.apollo247.testing.pages.MyAppointmentPage;
 import com.apollo247.testing.pages.LocationPage;
-//import com.apollo247.testing.pages.HealthInsurancePage;
 import com.apollo247.testing.pages.HealthtoolPage;
 
->>>>>>> e0e53877cf1731a16176a9c23f5898a1a35ce501
 
 
-import com.apollo247.testing.pages.ManageFamilyPage;
 import com.apollo247.testing.pages.SearchDoctorPage;
 import com.apollo247.testing.pages.MembershipsPage;
 import com.apollo247.testing.pages.LogoutPage;
@@ -33,9 +28,8 @@ public class Pages {
 
 	public static DashboardPage dashboardPage;
 	public static LabTestPage labTestPage;
-<<<<<<< HEAD
 
-	public static HealthInsurancePage healthInsurancePage;
+	//public static HealthInsurancePage healthInsurancePage;
 	
 
 	public static ManageFamilyPage manageFamilyPage; 
@@ -45,9 +39,7 @@ public class Pages {
 	public static NotificationsPage notificationsPage;
 	public static NeedHelpPage needHelpPage;
 	public static LogoutPage logoutPage;
-=======
 //	public static HealthInsurancePage healthInsurancePage;
-	public static SearchDoctorPage Searchdocter;
 //	public static ManageFamilyPage manageFamilyPage; 
 	public static UploadPrescriptionPage bookByPrescriptionPage;
 	public static RadiologyPage radiologyPage;
@@ -55,18 +47,16 @@ public class Pages {
 	public static MyAppointmentPage AppointmentDocter;
 	public static LocationPage LocationDocter;
 	public static HealthtoolPage HeartToolPage;
->>>>>>> e0e53877cf1731a16176a9c23f5898a1a35ce501
 
 	// all the pages are initialized with the driver object
 	public static void loadAllPages(WebDriver driver) {
 
 		// dashboard page driver initialization
 		dashboardPage = new DashboardPage(driver);
-<<<<<<< HEAD
 		Searchdocter = new SearchDoctorPage(driver);
 		manageFamilyPage = new ManageFamilyPage(driver);
 		myappointmentsPage = new MyAppointmentsPage(driver);
-		healthInsurancePage = new HealthInsurancePage(driver);
+	//	healthInsurancePage = new HealthInsurancePage(driver);
 		membershipsPage = new MembershipsPage(driver);
 		notificationsPage = new NotificationsPage(driver);
 		needHelpPage = new NeedHelpPage(driver);
@@ -75,7 +65,7 @@ public class Pages {
 		PageFactory.initElements(driver, dashboardPage);
 		PageFactory.initElements(driver, manageFamilyPage);
 		PageFactory.initElements(driver, myappointmentsPage);
-		PageFactory.initElements(driver, healthInsurancePage);
+	//	PageFactory.initElements(driver, healthInsurancePage);
 		PageFactory.initElements(driver, membershipsPage);
 		PageFactory.initElements(driver, notificationsPage);
 		PageFactory.initElements(driver, needHelpPage);
@@ -89,7 +79,6 @@ public class Pages {
 		PageFactory.initElements(driver, manageFamilyPage);
 		PageFactory.initElements(driver, myappointmentsPage);
 
-=======
 		Searchdocter=new SearchDoctorPage(driver);
 //	    manageFamilyPage = new ManageFamilyPage(driver);
 		labTestPage = PageFactory.initElements(driver, LabTestPage.class);
@@ -112,7 +101,6 @@ public class Pages {
 		// radiology page driver initialization
 		radiologyPage = new RadiologyPage(driver);
 		PageFactory.initElements(driver, radiologyPage);
->>>>>>> e0e53877cf1731a16176a9c23f5898a1a35ce501
 
 	}
 }
