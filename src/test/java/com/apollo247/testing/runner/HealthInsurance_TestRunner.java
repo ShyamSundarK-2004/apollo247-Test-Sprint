625fa65a723fec4c7a3c6feb561af09542693cc4
Gokul Anand S <sgokulanand0505@gmail.com>
2026-04-20 12:48:26 +0530

    Insurance conflicts Resolved

--- src/test/java/com/apollo247/testing/runner/HealthInsurance_TestRunner.java
+++ src/test/java/com/apollo247/testing/runner/HealthInsurance_TestRunner.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-	    features = {"./src/test/java/com/apollo247/testing/features/TS_INS_01.feature","./src/test/java/com/apollo247/testing/features/TS_INS_02.feature"},
+	    features = {"./src/test/java/com/apollo247/testing/features"},
 	    glue = {"com.apollo247.testing.stepdefinitions"},
 	    plugin = {
 	        "pretty",
