ba2294f37398bde168d24c5f31f1b87e89d0bd69
Shyam Sundar K <shyamson366@gmail.com>
2026-04-20 12:31:28 +0530

    parallel Execution - feature/labtest

--- src/main/java/com/apollo247/testing/pages/RadiologyPage.java
+++ src/main/java/com/apollo247/testing/pages/RadiologyPage.java
@@ -3,10 +3,8 @@ package com.apollo247.testing.pages;
 import java.util.List;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 
 import com.apollo247.testing.utilities.ActionUtilities;
